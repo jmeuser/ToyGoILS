d11d80b990ca0cd2029836afc888c9d61352998a
jmeuser <jmeuser@icloud.com>
2015-11-29 19:14:20 -0600

    Introduced Catalogue to hold a collection of books. Transformed "saveBooks()" into .save() method for Catalogues. Transformed "loadBooks()" into "loadCatalogue" to load a ccatalogue by name. Refactored main to test changes to catalogue system. For now I will us a universal catalogue, although it is possible in the future to make up a catalogue for each library, or for some other reason.

--- main.go
+++ main.go
@@ -11,37 +11,51 @@ type Book struct {
 	Title    string
 	ISBN     string
 	Library  string
-	ReqCount uint
+	ReqCount int
 }
 
-// loadBooks unmarshals data read from books.json
-func loadBooks() (*[]Book, error) {
-	data, err := ioutil.ReadFile("books.json")
-	if err != nil {
-		return nil, err
-	}
-	var books []Book
-	if err := json.Unmarshal(data, &books); err != nil {
-		return nil, fmt.Errorf("JSON unmarshling failed: %s", err)
-	}
-	return &books, nil
+type Catalogue struct {
+	Name  string
+	Count int
+	Books []*Book
 }
 
-// saveBooks marshals books then writes to books.json
-func saveBooks(books *[]Book) error {
-	data, err := json.Marshal(books)
+// save marshals .Books then writes to .Name + ".json"
+func (c *Catalogue) save() error {
+	data, err := json.Marshal(c.Books)
 	if err != nil {
 		return fmt.Errorf("JSON marshing failed: %s", err)
 	}
-	return ioutil.WriteFile("books.json", data, 0600)
+	filename := c.Name + ".json"
+	return ioutil.WriteFile(filename, data, 0600)
+}
+
+// loadCatalogue unmarshals data read from name.json into a Catalogue
+func loadCatalogue(name string) (*Catalogue, error) {
+	filename := name + ".json"
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	var books []*Book
+	if err := json.Unmarshal(data, &books); err != nil {
+		return nil, fmt.Errorf("JSON unmarshling failed: %s", err)
+	}
+	return &Catalogue{Name: name, Count: len(books), Books: books}, nil
 }
 
 func main() {
 	b0 := Book{Title: "Authority and the Individual", ISBN: "9781134812271", Library: "Pembrook Public Library"}
 	b1 := Book{Title: "The Principles of Mathematics", ISBN: "9780203864760", Library: "Pembrook Public Library"}
-	books := []Book{b0, b1}
-	saveBooks(&books)
-	b, _ := loadBooks()
-	fmt.Println(books)
-	fmt.Println(b)
+	books := []*Book{&b0, &b1}
+	UniCat := Catalogue{Name: "UniCat"}
+	UniCat.Books = books
+	UniCat.save()
+	for _, x := range UniCat.Books {
+		fmt.Println(x)
+	}
+	b, _ := loadCatalogue("UniCat")
+	for _, x := range b.Books {
+		fmt.Println(x)
+	}
 }
