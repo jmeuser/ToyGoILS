506bc56b1c5eb64748cc5ddab1c0b456edda3e99
jmeuser <jmeuser@icloud.com>
2015-11-29 20:25:10 -0600

    Introduced ./tmpl/book/view.html template. Introduced html/template for rendering book templates. Introduced net/http for running basic webserver. To Do:  Introduce edit book  Introduce view catalogue  Introduce search for book  Introduce search for book in catalogue  Introduce homepage  Eliminate possible race conditions using "synch" package and synch.Mutex or related tool.

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"net/http"
 )
 
 type Book struct {
@@ -44,18 +46,37 @@ func loadCatalogue(name string) (*Catalogue, error) {
 	return &Catalogue{Name: name, Count: len(books), Books: books}, nil
 }
 
+// templates cache
+var templates = template.Must(template.ParseFiles("./tmpl/book/view.html"))
+
+func renderBookTemplate(w http.ResponseWriter, tmpl string, b *Book) {
+	path := tmpl + ".html"
+	err := templates.ExecuteTemplate(w, path, b)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func viewBookHandler(w http.ResponseWriter, r *http.Request, b *Book) {
+	renderBookTemplate(w, "view", b) // place holder
+}
+
+func makeBookHandler(fn func(http.ResponseWriter, *http.Request, *Book)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fn(w, r, UniCat.Books[1]) // place holder
+	}
+}
+
+var UniCat Catalogue
+
 func main() {
 	b0 := Book{Title: "Authority and the Individual", ISBN: "9781134812271", Library: "Pembrook Public Library"}
 	b1 := Book{Title: "The Principles of Mathematics", ISBN: "9780203864760", Library: "Pembrook Public Library"}
 	books := []*Book{&b0, &b1}
-	UniCat := Catalogue{Name: "UniCat"}
+	UniCat = Catalogue{Name: "UniCat"}
 	UniCat.Books = books
 	UniCat.save()
-	for _, x := range UniCat.Books {
-		fmt.Println(x)
-	}
-	b, _ := loadCatalogue("UniCat")
-	for _, x := range b.Books {
-		fmt.Println(x)
-	}
+	http.HandleFunc("/", makeBookHandler(viewBookHandler))
+
+	http.ListenAndServe(":8080", nil)
 }
