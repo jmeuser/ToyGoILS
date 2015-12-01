894ac722e7456c0eaa790ea0691198a990c6c4f2
jmeuser <jmeuser@icloud.com>
2015-12-01 16:16:24 -0600

    Introduced find.html template Introduced findHandler and relevant code in main Introduced searchHandler and relevant code in main Introduced makeCatalogue from slice of pointers to books. Refactored loadCatalogue to call makeCatalogue Introduced maps to Catalogue to "represent" indexing along different columns e.g. Title or ISBN. It is likely that using maps to search in a catalogue for title and ISBN will be changed because at the moment the maps are from strings to slices of pointers to books. If they were maps of strings to bool then I could more easily combine tests to see if we even need to search a Catalogue's books without expending too much time or space. There is still some major refactoring to do before I consider that bottleneck, right now I'm just trying to create a fully functional toy.

--- main.go
+++ main.go
@@ -11,16 +11,20 @@ import (
 )
 
 type Book struct {
-	Title    string
-	ISBN     string
-	Library  string
-	ReqCount int
+	Title string
+	ISBN  string
+	Lib   string
+	Reqs  int
 }
 
+// Is it more efficient to use map[string]bool with search .Books method?
 type Catalogue struct {
-	Name  string
-	Count int
-	Books []*Book
+	Name   string
+	Count  int
+	Books  []*Book
+	Titles map[string][]*Book
+	ISBNs  map[string][]*Book
+	Libs   map[string][]*Book
 }
 
 // save marshals .Books then writes to .Name + ".json"
@@ -31,6 +35,18 @@ func (c *Catalogue) save() error {
 	}
 	filename := "./cats/" + c.Name + ".json"
 	return ioutil.WriteFile(filename, data, 0600)
+}
+
+func makeCatalogue(name string, books []*Book) *Catalogue {
+	titles := make(map[string][]*Book)
+	isbns := make(map[string][]*Book)
+	libs := make(map[string][]*Book)
+	for _, b := range books {
+		titles[b.Title] = append(titles[b.Title], b)
+		isbns[b.ISBN] = append(isbns[b.ISBN], b)
+		libs[b.Lib] = append(libs[b.Lib], b)
+	}
+	return &Catalogue{Name: name, Count: len(books), Books: books, Titles: titles, ISBNs: isbns, Libs: libs}
 }
 
 // loadCatalogue unmarshals data read from name.json into a Catalogue
@@ -44,14 +60,15 @@ func loadCatalogue(name string) (*Catalogue, error) {
 	if err := json.Unmarshal(data, &books); err != nil {
 		return nil, fmt.Errorf("JSON unmarshling failed: %s", err)
 	}
-	return &Catalogue{Name: name, Count: len(books), Books: books}, nil
+
+	return makeCatalogue(name, books), nil
 }
 
 // templates cache
-var templates = template.Must(template.ParseFiles("./tmpl/editBook.html", "./tmpl/viewBook.html", "./tmpl/viewCatalogue.html"))
+var templates = template.Must(template.ParseFiles("./tmpl/editBook.html", "./tmpl/viewBook.html", "./tmpl/viewCatalogue.html", "./tmpl/find.html"))
 
 func renderBookTemplate(w http.ResponseWriter, tmpl string, b *Book) {
-	err := templates.ExecuteTemplate(w, tmpl + ".html", b)
+	err := templates.ExecuteTemplate(w, tmpl+".html", b)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -61,7 +78,7 @@ func viewBookHandler(w http.ResponseWriter, r *http.Request, b *Book) {
 	renderBookTemplate(w, "viewBook", b) // place holder
 }
 
-func makeBookHandler(fn func(http.ResponseWriter, *http.Request, *Book)) http.HandlerFunc {
+func editBookHandler(fn func(http.ResponseWriter, *http.Request, *Book)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// get book from catalogue from url
 		// or return findBook (or refactor into find)
@@ -70,7 +87,7 @@ func makeBookHandler(fn func(http.ResponseWriter, *http.Request, *Book)) http.Ha
 }
 
 func renderCatalogueTemplate(w http.ResponseWriter, tmpl string, c *Catalogue) {
-	err := templates.ExecuteTemplate(w, tmpl + ".html", c)
+	err := templates.ExecuteTemplate(w, tmpl+".html", c)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -94,30 +111,57 @@ func viewHandler(w http.ResponseWriter, r *http.Request, url string) {
 	}
 }
 
+// findHandler presents the user with search options
+func findHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "find.html", nil) // placeholder
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	isbn := r.FormValue("isbn")
+	fmt.Println("\""+title+"\"", "\""+isbn+"\"")
+	c := &UniCat // placeholder
+	fmt.Println(*c)
+	if title != "" {
+		fmt.Println(c.Titles[title])
+		c = makeCatalogue("Results", c.Titles[title])
+	}
+	if isbn != "" {
+		c = makeCatalogue("Results", c.ISBNs[isbn])
+	}
+	fmt.Println(*c)
+	renderCatalogueTemplate(w, "viewCatalogue", c)
+}
+
+
+
 var validPath = regexp.MustCompile("^/view?.*$") // placeholder
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := validPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
-			http.NotFound(w,r)
+			http.NotFound(w, r)
 			return
 		}
 		fn(w, r, m[0]) // placeholder: send whole url onward
 	}
 }
 
-
 var UniCat Catalogue
 
 func main() {
-	b0 := Book{Title: "Authority and the Individual", ISBN: "9781134812271", Library: "Pembrook Public Library"}
-	b1 := Book{Title: "The Principles of Mathematics", ISBN: "9780203864760", Library: "Pembrook Public Library"}
+	b0 := Book{Title: "Authority and the Individual", ISBN: "9781134812271", Lib: "Pembrook Public Library"}
+	b1 := Book{Title: "The Principles of Mathematics", ISBN: "9780203864760", Lib: "Pembrook Public Library"}
 	books := []*Book{&b0, &b1}
-	UniCat = Catalogue{Name: "UniCat"}
-	UniCat.Books = books
+	UniCat = *makeCatalogue("UniCat", books)
 	UniCat.save()
 	http.HandleFunc("/view", makeHandler(viewHandler))
+	http.HandleFunc("/find", findHandler)
+	http.HandleFunc("/search", searchHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
